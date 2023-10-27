7fc4310ab066fe0786e262cd4e46732f1ebde020
exa18 <julian.cenkier@wp.eu>
2023-10-27 22:25:29 +0200

    update and fix
    Get users from file and copyto its homedirs.
    Fix to set copied files with its own permissions.

--- linux/bash/bashupdate.sh
+++ linux/bash/bashupdate.sh
@@ -2,15 +2,23 @@
 #
 sudo ls >/dev/null
 
-readarray -t arr <<< "$(ls -1 /home)"
+# list users with id 1000-1999 and if user info is filled
+readarray -t arr <<< "$(awk -F: '{if ($3>999 && $3<2000 && length($5)>0) print $1}' /etc/passwd)"
 
 lc=${#arr[@]}
 
-if [ $lc -gt 1 ]; then
+if [[ $lc -gt 1 ]]; then
 	for i in "${arr[@]}"; do
-		if [ ${i} != "${USER}" ]; then
-			sudo cp ~/.bashrc "/home/${i}/"
-			sudo cp ~/.bash_aliases "/home/${i}/"
+		if [[ ${i} != "${USER}" ]]; then
+			d="/home/${i}/"
+			if [[ -d "${d}" ]]; then
+				sudo cp ~/.bashrc "${d}"
+				sudo cp ~/.bash_aliases "${d}"
+				sudo chown -f $i:$i "${d}.bashrc"
+				sudo chown -f $i:$i "${d}.bash_aliases"
+				sudo chmod g=rw "${d}.bashrc"
+				sudo chmod g=rw "${d}.bash_aliases"
+			fi
 		fi
 	done
 fi
