384d1477b0d03d3f59312df69213561da199e8a8
exa18 <julian.cenkier@wp.eu>
2023-10-28 00:31:28 +0200

    update with cleaning

--- linux/bash/bashupdate.sh
+++ linux/bash/bashupdate.sh
@@ -6,23 +6,25 @@ sudo ls >/dev/null
 readarray -t arr <<< "$(awk -F: '{if ($3>999 && $3<2000 && length($5)>0) print $1}' /etc/passwd)"
 
 lc=${#arr[@]}
+rc=".bashrc"
+al=".bash_aliases"
 
 if [[ $lc -gt 1 ]]; then
 	for i in "${arr[@]}"; do
 		if [[ ${i} != "${USER}" ]]; then
 			d="/home/${i}/"
-			if [[ -d "${d}" ]]; then
-				sudo cp ~/.bashrc "${d}"
-				sudo cp ~/.bash_aliases "${d}"
-				sudo chown -f $i:$i "${d}.bashrc"
-				sudo chown -f $i:$i "${d}.bash_aliases"
-				sudo chmod g=rw "${d}.bashrc"
-				sudo chmod g=rw "${d}.bash_aliases"
+			if [[ -d $d ]]; then
+				sudo cp ~/$rc $d
+				sudo cp ~/$al $d
+				sudo chown -f $i:$i $d$rc
+				sudo chown -f $i:$i $d$al
+				sudo chmod g=rw $d$rc
+				sudo chmod g=rw $d$al
 			fi
 		fi
 	done
 fi
 
-	sudo cp ~/.bashrc "/root/"
-	sudo cp ~/.bash_aliases "/root/"
+	sudo cp ~/$rc /root/
+	sudo cp ~/$al /root/
 
