44fdbb9f45ecbc1f327555dbc417d8e053bafd82
exa18 <julian.cenkier@wp.eu>
2024-07-01 22:29:38 +0200

    swap fix
    Moved checking arg after check same arg IF wish off swap. This prevent error.

--- linux/bash/_unbundled/swap.sh
+++ linux/bash/_unbundled/swap.sh
@@ -5,11 +5,6 @@
 s='sudo '
 ${s}ls >/dev/null
 #
-# Check total ram and add 2GB as swapfile size
-gigs=$(echo "$(cat /proc/meminfo | grep MemTotal | awk '{print $2}') / 1000 / 1024 +2"|bc)
-if [[ -n "${1}" ]] && [[ $1 -gt 0 ]];then
-	gigs=$1
-fi
 # If swap ON then OFF
 sw=$(${s}swapon --show | grep "/swapfile" | wc -l)
 [[ $sw -gt 0 ]] && ${s}swapoff /swapfile
@@ -20,6 +15,11 @@ if [[ "${1}" == "--" ]];then
 	# and remove it from FSTAB
 	awk '!/\/swapfile/' /etc/fstab > tempswp && ${s}mv tempswp /etc/fstab 
 else
+	# Check total ram and add 2GB as swapfile size
+	gigs=$(echo "$(cat /proc/meminfo | grep MemTotal | awk '{print $2}') / 1000 / 1024 +2"|bc)
+	if [[ -n "${1}" ]] && [[ $1 -gt 0 ]];then
+		gigs=$1
+	fi
 	# Redefine swap
 	${s}dd if=/dev/zero of=/swapfile bs=1024 count=$((gigs * 1024 *1024)) status=progress
 	${s}chmod 600 /swapfile
