6b4285be7a50ab956aa26803b089b24fcb4dd5ef
exa18 <julian.cenkier@wp.eu>
2024-07-23 23:42:31 +0200

    update: nvidia hold
    Optimized show progress.
    Added hide cursor while main loop and restore on exit exit on trap.

--- linux/bash/_unbundled/nvidia_hold.sh
+++ linux/bash/_unbundled/nvidia_hold.sh
@@ -6,11 +6,23 @@ getinstalled() {
 }
 setonhold() {
 	if [[ "$(apt-mark showhold| grep ${p}|wc -l)" -eq "0" ]];then
-		echo "${1}${p}  /+/"
 		${s}apt-mark hold ${p} >/dev/null 2>&1
+		ch=$(echo "${lc} - $(echo ${p}|wc -c) + 3"|bc)
+		echo -e "\r${1}${p}  /+/$(spacefill)"
+		lc=0
 	fi
 }
-
+showpointer() {
+	tput cnorm
+}
+spacefill() {
+	# cover previous with spaces if shorter
+	lc=$(echo ${p}|wc -c)
+	while [[ "$ch" -ge "0" ]]; do
+		echo -n "."
+		ch=$((ch -1))
+	done
+}
 #
 #
 s='sudo '
@@ -20,20 +32,32 @@ ${s}ls >/dev/null
 ${s}apt-mark unhold $(apt-mark showhold)
 #
 getinstalled
+
+trap showpointer EXIT
+tput civis
 if [[ -n "${pkg[0]}" ]];then
+	t=${#pkg[@]}
+	x=1
+	lc=0
     for p in "${pkg[@]}"; do
 	if [[ -n "${p##*nvidia*}" ]];then
 		# If NOT contain "nvidia" then check dependencies
 		if [[ "$(apt depends ${p} 2>/dev/null|grep -P "Depends|Recomends"|grep -c nvidia)" -gt "0" ]];then
 			setonhold ":::"
 		else
-			echo "   ${p}"
+			echo -ne "\r${x}/${t}   ${p}"
+			ch=$(echo "${lc} - $(echo ${p}|wc -c)"|bc)
+			spacefill
 		fi
 	else
 		# nvidia detected
 		setonhold "..."
 	fi
+	x=$((x +1))
     done
 fi
 
-apt-mark showhold
+p="Completed."
+# compute spaces
+ch=$(echo "( ${lc} + $(echo ${x}${t}|wc -c) + 3 ) - $(echo ${p}|wc -c)"|bc)
+echo -ne "\r${p}$(spacefill)\n"
